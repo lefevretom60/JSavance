73b990e45e14face79ecf9febc07c500662c1477
lefevretom60 <113522471+lefevretom60@users.noreply.github.com>
2023-03-15 17:11:07 +0100

    Create main3.js

---
+++ main3.js
@@ -0,0 +1,16 @@
+function getRandom(){
+    
+    fetch('../mainapi.php').then((resp) => resp.json())
+    .then(function(data) {
+        
+        console.log(data);
+        UpdateDiv("arenne",data[0]);
+
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+function UpdateDiv(id,text){
+    var e = document.getElementById(id).innerHTML = text;
+}
